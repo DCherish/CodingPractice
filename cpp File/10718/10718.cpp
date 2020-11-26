e2b88f5900c84e1def3991357bdf6893c93780f2
DCherish <jongillou@gmail.com>
2020-11-26 23:58:54 +0900

    cpp File

---
+++ cpp File/10718/10718.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	for (int i = 0; i < 2; i++)
+	{
+		cout << "강한친구 대한육군\n";
+	}
+
+	return 0;
+}
