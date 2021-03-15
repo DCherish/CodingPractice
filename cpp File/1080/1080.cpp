fa8f1e86340dd7a8ac38fd79c0310f42eae90f9a
DCherish <jongillou@gmail.com>
2021-03-15 13:47:49 +0900

    cpp File

--- cpp File/1080/1080.cpp
+++ cpp File/1080/1080.cpp
@@ -74,17 +74,6 @@ int main()
 		}
 	}
 
-	for (int i = 0; i < N - 2; i++)
-	{
-		for (int j = 0; j < M - 2; j++)
-		{
-			if (be_matrix[i][j] != af_matrix[i][j])
-			{
-				change(i, j);
-			}
-		}
-	}
-
 	if (compare() == true)
 	{
 		cout << cnt << "\n";
