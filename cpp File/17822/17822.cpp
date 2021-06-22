5892d60d6c08a698d3af65c3617dea7322880e6e
DCherish <jongillou@gmail.com>
2021-06-22 23:23:21 +0900

    cpp File

---
+++ cpp File/17822/17822.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int N, M, T, num;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N >> M >> T;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> num;
+		}
+	}
+
+	return 0;
+}
