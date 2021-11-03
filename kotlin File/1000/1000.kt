9d576bb40cd708c3fbd37833b297c5e63466e7ba
DCherish <jongillou@gmail.com>
2021-11-03 23:48:20 +0900

    kt File

--- kotlin File/1000/1000.kt
+++ kotlin File/1000/1000.kt
@@ -1,9 +1,5 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val (A, B) = br.readLine().split(' ').map { it.toInt() }
+    val (A, B) = readLine()!!.split(' ')
 
-    println(A + B)
+    println(A.toInt() + B.toInt())
 }
