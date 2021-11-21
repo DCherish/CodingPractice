2eef44f5817f6574e7ffe0e43dc5d7542528b7e7
DCherish <jongillou@gmail.com>
2021-11-22 00:10:23 +0900

    kt File

--- kotlin File/10828.kt
+++ kotlin File/10828.kt
@@ -4,11 +4,38 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
+    val stk = ArrayDeque<String>()
+
     val N = br.readLine()!!.toInt()
 
     for (i in 0 until N) {
         val s = br.readLine()!!.toString()
 
-        println(s.substring(0, 4))
+        if (s.contains("push")) {
+            stk.addLast(s.substring(5))
+        } else if (s.contains("pop")) {
+            if (stk.isEmpty()) {
+                bw.write("-1\n");
+            } else {
+                bw.write("${stk.last()}\n")
+                stk.removeLast()
+            }
+        } else if (s == "size") {
+            bw.write("${stk.size}\n")
+        } else if (s == "empty") {
+            if (stk.isEmpty()) {
+                bw.write("1\n")
+            } else {
+                bw.write("0\n")
+            }
+        } else if (s == "top") {
+            if (stk.isEmpty()) {
+                bw.write("-1\n")
+            } else {
+                bw.write("${stk.last()}\n")
+            }
+        }
     }
+
+    bw.flush()
 }
