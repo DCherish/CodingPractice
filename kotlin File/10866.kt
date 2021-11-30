b803beaf590aecba5fefbdd8274f93b51bece1f5
DCherish <jongillou@gmail.com>
2021-11-30 22:59:56 +0900

    kt File

--- kotlin File/10866.kt
+++
@@ -1,8 +0,0 @@
-import java.io.*
-
-fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-
-    val N = br.readLine()!!.toInt()
-}
