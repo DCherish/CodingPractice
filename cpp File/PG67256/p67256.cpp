b8234b762338dcb6c19bf8e70b372e23defb4a88
DCherish <jongillou@gmail.com>
2022-09-05 18:33:40 +0900

    cpp File

--- cpp File/PG67256/p67256.cpp
+++ cpp File/PG67256/p67256.cpp
@@ -12,32 +12,32 @@ struct pnt
 
 pnt l, r;
 
-int keyx[12] = { 3, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3 };
-int keyy[12] = { 1, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 2 };
+int keyx[] = {3, 0, 0, 0, 1, 1, 1, 2, 2, 2};
+int keyy[] = {1, 0, 1, 2, 0, 1, 2, 0, 1, 2};
 
 string solution(vector<int> numbers, string hand)
 {
     string answer = "";
-    
+
     l.x = 3;
     l.y = 0;
-    
+
     r.x = 3;
     r.y = 2;
-    
+
     for (int i = 0; i < numbers.size(); i++)
     {
         if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7)
         {
             answer += "L";
-            
+
             l.x = keyx[numbers[i]];
             l.y = keyy[numbers[i]];
         }
         else if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9)
         {
             answer += "R";
-            
+
             r.x = keyx[numbers[i]];
             r.y = keyy[numbers[i]];
         }
@@ -45,18 +45,18 @@ string solution(vector<int> numbers, string hand)
         {
             int ld = abs(keyx[numbers[i]] - l.x) + abs(keyy[numbers[i]] - l.y);
             int rd = abs(keyx[numbers[i]] - r.x) + abs(keyy[numbers[i]] - r.y);
-            
+
             if (ld < rd)
             {
                 answer += "L";
-            
+
                 l.x = keyx[numbers[i]];
                 l.y = keyy[numbers[i]];
             }
             else if (ld > rd)
             {
                 answer += "R";
-            
+
                 r.x = keyx[numbers[i]];
                 r.y = keyy[numbers[i]];
             }
@@ -66,20 +66,20 @@ string solution(vector<int> numbers, string hand)
                 if (hand == "left")
                 {
                     answer += "L";
-            
+
                     l.x = keyx[numbers[i]];
                     l.y = keyy[numbers[i]];
                 }
                 else
                 {
                     answer += "R";
-            
+
                     r.x = keyx[numbers[i]];
                     r.y = keyy[numbers[i]];
                 }
             }
         }
     }
-    
+
     return answer;
 }
