94b0344292b22f2a30f43f23906f37f2d7c9c4ab
DCherish <jongillou@gmail.com>
2022-09-07 17:04:44 +0900

    cpp File

--- cpp File/PG60057/p60057.cpp
+++ cpp File/PG60057/p60057.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(string s)
 {
 	int answer = 0;
-	int maxlen = s.length();
+	int minlen = s.length();
 
 	int len = s.length() / 2;
 
@@ -15,6 +15,7 @@ int solution(string s)
 	{
 		string prev = s.substr(0, i);
 		answer = i;
+        
 		int cnt = 1;
 
 		for (int j = i; j < s.length(); j = j + i)
@@ -43,35 +44,10 @@ int solution(string s)
 			answer += to_string(cnt).length();
 		}
 
-		if (answer < maxlen) maxlen = answer;
+		minlen = min(minlen, answer);
 	}
 
-	answer = maxlen;
+	answer = minlen;
 
 	return answer;
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	string s1 = "aabbaccc";
-	string s2 = "ababcdcdababcdcd";
-	string s3 = "abcabcdede";
-	string s4 = "abcabcabcabcdededededede";
-	string s5 = "xababcdcdababcdcd";
-	string s6 = "aaaaaaaaaab";
-	string s7 = "a";
-
-	cout << solution(s1) << "\n";
-	cout << solution(s2) << "\n";
-	cout << solution(s3) << "\n";
-	cout << solution(s4) << "\n";
-	cout << solution(s5) << "\n";
-	cout << solution(s6) << "\n";
-	cout << solution(s7) << "\n";
-
-	return 0;
 }
