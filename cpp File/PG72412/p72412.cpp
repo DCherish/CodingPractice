83ac1dec05f4b5e6ef80972aff153f00a46e8566
DCherish <jongillou@gmail.com>
2022-09-08 17:59:18 +0900

    cpp File

--- cpp File/PG72412/p72412.cpp
+++ cpp File/PG72412/p72412.cpp
@@ -16,31 +16,29 @@ vector<int> solution(vector<string> info, vector<string> query)
         stringstream ss(info[i]);
         string token;
         
-        vector<int> vec;
+        vector<string> temp;
         
         while (getline(ss, token, ' '))
         {
-            if (token == "java" || token == "backend"
-                || token == "junior" || token == "pizza")
-            {
-                vec.push_back(0);
-            }
-            else if (token == "python" || token == "frontend"
-                || token == "senior" || token == "chicken")
-            {
-                vec.push_back(1);
-            }
-            else if (token == "cpp")
-            {
-                vec.push_back(2);
-            }
-            else
-            {
-                vec.push_back(stoi(token));
-            }
+            temp.push_back(token);
         }
         
-        db[vec[0]][vec[1]][vec[2]][vec[3]].push_back(vec[4]);
+        int a, b, c, d;
+        
+        if (temp[0] == "cpp") a = 0;
+        else if (temp[0] == "java") a = 1;
+        else if (temp[0] == "python") a = 2;
+        
+        if (temp[1] == "backend") b = 0;
+        else if (temp[1] == "frontend") b = 1;
+        
+        if (temp[2] == "junior") c = 0;
+        else if (temp[2] == "senior") c = 1;
+        
+        if (temp[3] == "chicken") d = 0;
+        else if (temp[3] == "pizza") d = 1;
+        
+        db[a][b][c][d].push_back(stoi(temp[4]));
     }
     
     for (int i = 0; i < 3; i++)
@@ -59,116 +57,93 @@ vector<int> solution(vector<string> info, vector<string> query)
     
     for (int i = 0; i < query.size(); i++)
     {
-        vector<int> vec;
-
-        int a1, a2, b1, b2, c1, c2, d1, d2, score;
-
         stringstream ss(query[i]);
         string token;
-
+        
+        vector<string> temp;
+        
         while (getline(ss, token, ' '))
         {
-            if (token == "java" || token == "backend"
-                || token == "junior" || token == "pizza")
-            {
-                vec.push_back(0);
-            }
-            else if (token == "python" || token == "frontend"
-                || token == "senior" || token == "chicken")
-            {
-                vec.push_back(1);
-            }
-            else if (token == "cpp")
-            {
-                vec.push_back(2);
-            }
-            else if (token == "-")
-            {
-                vec.push_back(3);
-            }
-            else if (token == "and")
-            {
-                continue;
-            }
-            else
-            {
-                vec.push_back(stoi(token));
-            }
+            if (token == "and") continue;
+            
+            temp.push_back(token);
         }
-
-        if (vec[0] == 0) // java
+        
+        int a1, a2, b1, b2, c1, c2, d1, d2, score;
+        
+        if (temp[0] == "cpp")
         {
             a1 = 0;
             a2 = 0;
         }
-        else if (vec[0] == 1) // python
+        else if (temp[0] == "java")
         {
             a1 = 1;
             a2 = 1;
         }
-        else if (vec[0] == 2) // cpp
+        else if (temp[0] == "python")
         {
             a1 = 2;
             a2 = 2;
         }
-        else if (vec[0] == 3) // -
+        else
         {
             a1 = 0;
             a2 = 2;
         }
-
-        if (vec[1] == 0) // backend
+        
+        if (temp[1] == "backend")
         {
             b1 = 0;
             b2 = 0;
         }
-        else if (vec[1] == 1) // frontend
+        else if (temp[1] == "frontend")
         {
             b1 = 1;
             b2 = 1;
         }
-        else if (vec[1] == 3) // -
+        else
         {
             b1 = 0;
             b2 = 1;
         }
-
-        if (vec[2] == 0) // junior
+        
+        if (temp[2] == "junior")
         {
             c1 = 0;
             c2 = 0;
         }
-        else if (vec[2] == 1) // senior
+        else if (temp[2] == "senior")   
         {
             c1 = 1;
             c2 = 1;
         }
-        else if (vec[2] == 3) // -
+        else
         {
             c1 = 0;
             c2 = 1;
         }
-
-        if (vec[3] == 0) // pizza
+        
+        if (temp[3] == "chicken")
         {
             d1 = 0;
             d2 = 0;
         }
-        else if (vec[3] == 1) // chicken
+        else if (temp[3] == "pizza")
         {
             d1 = 1;
             d2 = 1;
         }
-        else if (vec[3] == 3) // -
+        else
         {
             d1 = 0;
             d2 = 1;
         }
-
-        score = vec[4];
         
-        int temp = 0;
-
+        score = stoi(temp[4]);
+        
+        int cnt = 0;
+        
         for (int a = a1; a <= a2; a++)
         {
             for (int b = b1; b <= b2; b++)
@@ -178,18 +153,18 @@ vector<int> solution(vector<string> info, vector<string> query)
                     for (int d = d1; d <= d2; d++)
                     {
                         int n = db[a][b][c][d].size();
-
+                        
                         if (n == 0) continue;
-
+                        
                         if (lower_bound(db[a][b][c][d].begin(), db[a][b][c][d].end(), score) == db[a][b][c][d].end()) continue;
-                        else temp += n - (lower_bound(db[a][b][c][d].begin(), db[a][b][c][d].end(), score) - db[a][b][c][d].begin());
+                        else cnt += n - (lower_bound(db[a][b][c][d].begin(), db[a][b][c][d].end(), score) - db[a][b][c][d].begin());
                     }
                 }
             }
         }
-
-        answer.push_back(temp);
+        
+        answer.push_back(cnt);
     }
-
+    
     return answer;
 }
