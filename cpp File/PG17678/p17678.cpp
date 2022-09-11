4a608afb37bcb9285913d9fa09515567af6ec67e
DCherish <jongillou@gmail.com>
2022-09-12 00:30:24 +0900

    cpp File

--- cpp File/PG17678/p17678.cpp
+++ cpp File/PG17678/p17678.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <sstream>
@@ -8,15 +7,15 @@ using namespace std;
 
 vector<int> vec;
 
-void cal(vector<int> temp)
+int cnvert(string s)
 {
-	int time = 0;
+	int result = 0;
 
-	time += temp[0] * 60;
+	result += stoi(s.substr(0, 2)) * 60;
 
-	time += temp[1];
+	result += stoi(s.substr(3, 2));
 
-	vec.push_back(time);
+	return result;
 }
 
 string solution(int n, int t, int m, vector<string> timetable)
@@ -25,19 +24,11 @@ string solution(int n, int t, int m, vector<string> timetable)
 
 	int min_time = 540;
 
-	for (int i = 0; i < timetable.size(); i++) // �� ���
+	for (int i = 0; i < timetable.size(); i++)
 	{
-		vector<int> temp;
-
-		stringstream ss(timetable[i]);
-		string token;
-
-		while (getline(ss, token, ':'))
-		{
-			temp.push_back(stoi(token));
-		}
-
-		cal(temp);
+		int time = cnvert(timetable[i]);
+        
+        vec.push_back(time);
 	}
 
 	sort(vec.begin(), vec.end());
@@ -63,14 +54,8 @@ string solution(int n, int t, int m, vector<string> timetable)
 
 		if (i + 1 == n)
 		{
-			if (full == 0)
-			{
-				f_time = vec[idx - 1] - 1;
-			}
-			else
-			{
-				f_time = min_time;
-			}
+			if (full == 0) f_time = vec[idx - 1] - 1;
+			else f_time = min_time;
 		}
 
 		min_time += t;
@@ -79,47 +64,13 @@ string solution(int n, int t, int m, vector<string> timetable)
 	int hr = f_time / 60;
 	int mt = f_time % 60;
 
-	if (hr < 10)
-	{
-		answer += "0" + to_string(hr);
-	}
-	else
-	{
-		answer += to_string(hr);
-	}
+	if (hr < 10) answer += ("0" + to_string(hr));
+	else answer += to_string(hr);
 
 	answer += ":";
 
-	if (mt < 10)
-	{
-		answer += "0" + to_string(mt);
-	}
-	else
-	{
-		answer += to_string(mt);
-	}
+	if (mt < 10) answer += ("0" + to_string(mt));
+	else answer += to_string(mt);
 
 	return answer;
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	int n, t, m;
-	vector<string> timetable;
-
-	cin >> n >> t >> m;
-
-	timetable.push_back("09:10");
-	timetable.push_back("09:09");
-	timetable.push_back("08:00");
-
-	string str = solution(n, t, m, timetable);
-
-	cout << str << "\n";
-
-	return 0;
 }
