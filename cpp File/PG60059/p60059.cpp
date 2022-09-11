caac8f3d1af9757206fe758877ee22a6f0921935
DCherish <jongillou@gmail.com>
2022-09-11 20:43:21 +0900

    cpp File

--- cpp File/PG60059/p60059.cpp
+++ cpp File/PG60059/p60059.cpp
@@ -3,12 +3,16 @@
 
 using namespace std;
 
-int keysz, locksz, boardsz;
+int keysz = 0;
+int locksz = 0;
+int boardsz = 0;
 
 vector<vector<int>> r_key;
 
-void rotate_key(vector<vector<int>> key)
+void rotate_key()
 {
+    vector<vector<int>> key = r_key;
+    
     r_key.clear();
 
     for (int i = 0; i < keysz; i++)
@@ -24,70 +28,64 @@ void rotate_key(vector<vector<int>> key)
     }
 }
 
-bool put_key(int x, int y, vector<vector<int>> key, vector<vector<int>> board)
+bool check(int x, int y, vector<vector<int>> board)
 {
+    int keysz = r_key.size();
+    
     for (int i = x; i < x + keysz; i++)
     {
         for (int j = y; j < y + keysz; j++)
         {
-            board[i][j] += key[i - x][j - y];
+            board[i][j] += r_key[i - x][j - y];
         }
     }
-
-    for (int i = keysz - 1; i <= boardsz - keysz; i++)
+    
+    for (int i = keysz - 1; i < boardsz - keysz + 1; i++)
     {
-        for (int j = keysz - 1; j <= boardsz - keysz; j++)
+        for (int j = keysz - 1; j < boardsz - keysz + 1; j++)
         {
-            if (board[i][j] == 1) continue;
-
-            return false;
+            if (board[i][j] != 1) return false;
         }
     }
-
+    
     return true;
 }
 
 bool solution(vector<vector<int>> key, vector<vector<int>> lock)
 {
     bool answer = false;
-
+    
     keysz = key.size();
     locksz = lock.size();
-    boardsz = locksz + (keysz - 1) * 2;
-
-    //vector<int> temp(5, 3);
-    vector<vector<int>> board(boardsz, vector<int>(boardsz, 0));
-    // 벡터 직접 접근 및 초기화
-
-    for (int i = keysz - 1; i <= boardsz - keysz; i++)
+    boardsz = keysz * 2 + locksz - 2;
+    
+    vector<vector<int>> board(boardsz, vector<int>(boardsz));
+    
+    for (int i = keysz - 1; i < keysz - 1 + locksz; i++)
     {
-        for (int j = keysz - 1; j <= boardsz - keysz; j++)
+        for (int j = keysz - 1; j < keysz - 1 + locksz; j++)
         {
             board[i][j] = lock[i - keysz + 1][j - keysz + 1];
         }
     }
-
+    
     for (int i = 0; i < keysz; i++)
     {
         r_key.push_back(key[i]);
     }
-
+    
     for (int r = 0; r < 4; r++)
     {
         for (int i = 0; i <= boardsz - keysz; i++)
         {
-            for (int j = 0; j < boardsz - keysz; j++)
+            for (int j = 0; j <= boardsz - keysz; j++)
             {
-                if (put_key(i, j, r_key, board))
-                {
-                    answer = true;
-                    return answer;
-                }
+                if (check(i, j, board)) return true;
             }
         }
 
-        rotate_key(r_key);
+        rotate_key();
     }
-
+    
     return answer;
 }
