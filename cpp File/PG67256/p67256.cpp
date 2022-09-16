d97fd296f23ffdb7e7ab6029cd89cecea839ff0d
DCherish <jongillou@gmail.com>
2022-09-16 16:06:59 +0900

    cpp File

--- cpp File/PG67256/p67256.cpp
+++ cpp File/PG67256/p67256.cpp
@@ -4,61 +4,55 @@
 
 using namespace std;
 
-struct pnt
+int dx[] = {3, 0, 0, 0, 1, 1, 1, 2, 2, 2};
+int dy[] = {1, 0, 1, 2, 0, 1, 2, 0, 1, 2};
+
+int cal(int x, int y, int num)
 {
-    int x;
-    int y;
-};
-
-pnt l, r;
-
-int keyx[] = {3, 0, 0, 0, 1, 1, 1, 2, 2, 2};
-int keyy[] = {1, 0, 1, 2, 0, 1, 2, 0, 1, 2};
+    return abs(dx[num] - x) + abs(dy[num] - y);
+}
 
 string solution(vector<int> numbers, string hand)
 {
     string answer = "";
-
-    l.x = 3;
-    l.y = 0;
-
-    r.x = 3;
-    r.y = 2;
-
+    
+    int lx = 3;
+    int ly = 0;
+    
+    int rx = 3;
+    int ry = 2;
+    
     for (int i = 0; i < numbers.size(); i++)
     {
         if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7)
         {
             answer += "L";
-
-            l.x = keyx[numbers[i]];
-            l.y = keyy[numbers[i]];
+            
+            lx = dx[numbers[i]];
+            ly = dy[numbers[i]];
         }
         else if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9)
         {
             answer += "R";
-
-            r.x = keyx[numbers[i]];
-            r.y = keyy[numbers[i]];
+            
+            rx = dx[numbers[i]];
+            ry = dy[numbers[i]];
         }
         else
         {
-            int ld = abs(keyx[numbers[i]] - l.x) + abs(keyy[numbers[i]] - l.y);
-            int rd = abs(keyx[numbers[i]] - r.x) + abs(keyy[numbers[i]] - r.y);
-
-            if (ld < rd)
+            if (cal(lx, ly, numbers[i]) < cal(rx, ry, numbers[i]))
             {
                 answer += "L";
-
-                l.x = keyx[numbers[i]];
-                l.y = keyy[numbers[i]];
+            
+                lx = dx[numbers[i]];
+                ly = dy[numbers[i]];
             }
-            else if (ld > rd)
+            else if (cal(lx, ly, numbers[i]) > cal(rx, ry, numbers[i]))
             {
                 answer += "R";
-
-                r.x = keyx[numbers[i]];
-                r.y = keyy[numbers[i]];
+            
+                rx = dx[numbers[i]];
+                ry = dy[numbers[i]];
             }
             else
             {
@@ -66,20 +60,20 @@ string solution(vector<int> numbers, string hand)
                 if (hand == "left")
                 {
                     answer += "L";
-
-                    l.x = keyx[numbers[i]];
-                    l.y = keyy[numbers[i]];
+            
+                    lx = dx[numbers[i]];
+                    ly = dy[numbers[i]];
                 }
                 else
                 {
                     answer += "R";
-
-                    r.x = keyx[numbers[i]];
-                    r.y = keyy[numbers[i]];
+            
+                    rx = dx[numbers[i]];
+                    ry = dy[numbers[i]];
                 }
             }
         }
     }
-
+    
     return answer;
 }
